607e4acc69e481df9c6c8d31af126326533b2f24
Dinmukhamet <igissinov_d@auca.kg>
2021-11-09 17:24:17 +0600

    fix: change data type and handle base name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from typing import Any, Dict
 import aiofiles
 from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.responses import FileResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Json
 
 import config
 from convert import Convert
@@ -15,7 +15,7 @@ app = FastAPI()
 
 class Data(BaseModel):
     file_path: str
-    content: Dict[str, Any]
+    content: Json
 
 
 @app.post("/upload-template")
@@ -31,6 +31,18 @@ async def upload_template(file: UploadFile = File(...)):
 def download_converted(data: Data):
     rendered_file: Path = Render.render_file(data.file_path, data.content)
     converted_file: Path = Convert.convert_file(rendered_file)
+    if not converted_file.exists():
+        raise HTTPException(status_code=500, detail="Failed to convert file.")
+    return converted_file
+
+
+@app.post("/convert", response_class=FileResponse)
+async def convert(file: UploadFile = File(...)):
+    file_path = Path(config.RENDERED_DIR / file.filename)
+    async with aiofiles.open(file_path, "wb") as out_file:
+        content = await file.read()
+        await out_file.write(content)
+    converted_file: Path = Convert.convert_file(file_path)
     if not converted_file.exists():
         raise HTTPException(status_code=500, detail="Failed to convert file.")
     return converted_file
--- render.py
+++ render.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 from typing import Any, Dict
+from uuid import uuid4
 
 from docxtpl import DocxTemplate
 
@@ -14,11 +15,14 @@ logger.setLevel(level=logging.INFO)
 class Render:
     @classmethod
     def render_file(cls, file_path: str, context: Dict[str, Any]) -> Path:
+        logger.info(f"Received context: {context}")
+
         doc = DocxTemplate(file_path)
         doc.render(context)
 
-        
-        file_path = cls.get_path(base_name=context.get("FullName") or context.get("CashDocumentNumber"))
+        file_path = cls.get_path(
+            base_name=context.get("FullName") or context.get("CashDocumentNumber")
+        )
 
         logger.info("Saving rendered file to: {path}".format(path=file_path))
         doc.save(file_path)
@@ -26,6 +30,8 @@ class Render:
 
     @classmethod
     def get_path(cls, base_name: str = None) -> Path:
+        if base_name is None:
+            base_name = str(uuid4())
         path = Path(RENDERED_DIR / base_name.lower().replace(" ", "-"))
         path.mkdir(parents=True, exist_ok=True)
         return path.joinpath("rendered-agreement.docx")
